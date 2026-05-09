6fa02154d0947b02ad9ea61d34ef3b829395e892
KhadijaAli <alloughanikhadijaali@gmail.com>
2026-05-09 21:16:35 +0400

    Admin Module

---
+++ src/main/java/Admin_Module.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Admin_Module {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        boolean AdminRunning = true;
+
+
+    }
+}
