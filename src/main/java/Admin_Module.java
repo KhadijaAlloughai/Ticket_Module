034c644cc7fe728ecc23453cf9502ecf90bbe776
KhadijaAli <alloughanikhadijaali@gmail.com>
2026-05-09 22:13:45 +0400

    Hashmap

--- src/main/java/Admin_Module.java
+++ src/main/java/Admin_Module.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Admin_Module {
@@ -6,6 +7,14 @@ public class Admin_Module {
 
         Scanner input = new Scanner(System.in);
         boolean AdminRunning = true;
+
+        // LIST ID AND COMPLAINTS
+        HashMap<Integer, String> complaints = new HashMap<Integer, String>();
+        complaints.put(101, " Long Wait Times");
+        complaints.put(102, " Poor Product Quality");
+        complaints.put(103, " Misleading Product Descriptions");
+        complaints.put(104, " Poor Product Quality");
+        complaints.put(105, " Misleading Product Details");
 
 
     }
